b47e1e21481e624b7f099213ac4f8ada38512b9f
umer muazzam <umer.muazzam@magnusmage.com>
2024-08-23 18:04:40 +0500

    home design

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,17 +1,25 @@
-import React from 'react' 
-import Image from 'next/image'
+import React from "react";
+import heroImg from "public/hero.png";
+import Image from "next/image";
+import styles from "./page.module.css";
 
 const page = () => {
   return (
-    <div>
-      <Image 
-        src={
-          "https://cdn.pixabay.com/photo/2022/10/24/12/20/mountains-7543273_1280.jpg"
-        } width={400} height={400}
-        alt="hero image"
-      />
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <h1 className={styles.title}>Better design for your digital product</h1>
+        <p className={styles.desc}>
+          {" "}
+          Turning your idea to reality, we bring the team from the glob tech
+          industry
+        </p>
+        <button className={styles.button}>See Our Works</button>
+      </div>
+      <div className={styles.item}>
+        <Image src={heroImg} className={styles.img} alt="hero image" />
+      </div>
     </div>
   );
-}
+};
 
-export default page
+export default page;
